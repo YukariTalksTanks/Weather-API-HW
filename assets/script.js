19eb93bad930958bb5e1e719c1c3aeb45bff8d2c
Preston Bradford <psbradford4100@att.net>
2020-06-22 15:50:44 -0400

    stuff

--- assets/script.js
+++ assets/script.js
@@ -14,7 +14,7 @@ function searchsubmit() {
         method: "GET"
     })
         .then(function (response) {
-            console.clear();
+            // console.clear();
             var main = response.main;
             var k = main.temp;
             var tempF = Math.floor(((k - 273.15) * 1.8) + 32);
@@ -66,9 +66,12 @@ function searchsubmit() {
             console.log("5 day forcast: ", response);
             // I need to dig into the list array in the 5 day forcast console. but that shish is hard
             // also its 1am so ill take a break now for sleep. Ill see if I can get help in office hours.
-            for (var i = 0; i <= 5; i++) {
+            for (var i = 0; i < 40; i++) {
                 var list = response.list[i];
-                var main = list[i].main;
+                // console.log(list);
+                if (list.dt_txt.indexOf("15:00:00") !== -1) {
+                    console.log(list);
+                    var main = list.main;
                 var k = main.temp;
                 var tempF = Math.floor(((k - 273.15) * 1.8) + 32);
                 var temperature = "Temperature: " + tempF;
@@ -76,7 +79,23 @@ function searchsubmit() {
                 var Humidity = main.humidity;
 
                 var wind = response.wind;
-                console.log("this is forecast temp: ", temperature1)
+                console.log("this is forecast temp: ", temperature)
+                var colHold = $("<div>").addClass("col-sm");
+                var minBox = $("<div>").addClass("mini-box");
+                var pTemp = $("<p>").text(temperature);
+
+                $("#daycast").append(colHold.append(minBox.append(pTemp)))
+                }
+
+                // var main = list.main;
+                // var k = main.temp;
+                // var tempF = Math.floor(((k - 273.15) * 1.8) + 32);
+                // var temperature = "Temperature: " + tempF;
+
+                // var Humidity = main.humidity;
+
+                // var wind = response.wind;
+                // console.log("this is forecast temp: ", temperature1)
             }
             // var main1 = response.list.main;
             // var k1 = main1.temp;
